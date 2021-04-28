9ea09ba3cc4a0767ee8905f59c0dcf36fdc9fadf
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-28 14:45:24 -0600

    Create Pizza.java

---
+++ src/Pizza.java
@@ -0,0 +1,23 @@
+/*
+    For all intents and purposes, the Pizza class can be treated as a Cheese Pizza, as mozzarella and tomato sauce are on every pizza.
+*/
+
+public class Pizza extends AbstractPizza
+{
+    public Pizza()
+    {
+        ingredientCount = 2;
+    }
+  
+    @Override
+    public double getPrice()
+    {
+        return 5;   
+    }
+    
+    @Override
+    public void printIngredients() // The only default ingredients are mozzarella cheese and tomato sauce.
+    {
+        System.out.print("Pizza Ingredients: Mozzarella Cheese, Tomato Sauce, ");   
+    }
+}
