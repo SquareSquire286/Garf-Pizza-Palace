b1474ae416074c0cda625bd11d4b1eaba85c2d42
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-28 13:21:30 -0600

    Create Fettucine.java

---
+++ src/Fettucine.java
@@ -0,0 +1,14 @@
+public class Fettucine extends Pasta
+{
+    @Override
+    public double getPrice()
+    {
+        return 4.25;
+    }
+    
+    @Override
+    public String getSupplier()
+    {
+        return "Barilla";
+    }   
+}
