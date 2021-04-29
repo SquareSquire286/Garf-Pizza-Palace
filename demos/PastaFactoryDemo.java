6eff364b356f656f25df86c36176db33f11b8985
Jacob Sauer <55856650+SquareSquire286@users.noreply.github.com>
2021-04-29 15:52:47 -0600

    Update PastaFactoryDemo.java

--- demos/PastaFactoryDemo.java
+++ demos/PastaFactoryDemo.java
@@ -10,11 +10,11 @@ public class PastaFactoryDemo
         Pasta pasta4 = pastaFactory.createPasta(4); // should create a Penne object
         Pasta pasta5 = pastaFactory.createPasta(5); // should create a Lasagna object
             
-        System.out.println("Pasta Information: " + pasta1.getSupplier() + ", " + String.format("%.2f", pasta1.getPrice())); // should print "Catelli, $3.50"
-        System.out.println("Pasta Information: " + pasta2.getSupplier() + ", " + String.format("%.2f", pasta2.getPrice())); // should print "Barilla, $4.25"
-        System.out.println("Pasta Information: " + pasta3.getSupplier() + ", " + String.format("%.2f", pasta3.getPrice())); // should print "Catelli, $4.00"
-        System.out.println("Pasta Information: " + pasta4.getSupplier() + ", " + String.format("%.2f", pasta4.getPrice())); // should print "Catelli, $4.50"
-        System.out.println("Pasta Information: " + pasta5.getSupplier() + ", " + String.format("%.2f", pasta5.getPrice())); // should print "Primo, $5.00"
+        System.out.println("Pasta Information: " + pasta1.getSupplier() + ", $" + String.format("%.2f", pasta1.getPrice())); // should print "Catelli, $3.50"
+        System.out.println("Pasta Information: " + pasta2.getSupplier() + ", $" + String.format("%.2f", pasta2.getPrice())); // should print "Barilla, $4.25"
+        System.out.println("Pasta Information: " + pasta3.getSupplier() + ", $" + String.format("%.2f", pasta3.getPrice())); // should print "Catelli, $4.00"
+        System.out.println("Pasta Information: " + pasta4.getSupplier() + ", $" + String.format("%.2f", pasta4.getPrice())); // should print "Catelli, $4.50"
+        System.out.println("Pasta Information: " + pasta5.getSupplier() + ", $" + String.format("%.2f", pasta5.getPrice())); // should print "Primo, $5.00"
         
         return;
     }
